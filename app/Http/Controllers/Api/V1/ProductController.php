cc65fea1de94223a8a951e1690601ca00f73ac19
Andre Ricarti <a.ricarti@qiwi.com>
2017-07-18 18:47:58 -0300

    Continuação dos estudos sobre api com laravel.

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,6 +9,7 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     private $product;
+    private $totalPage = 5;
 
     public function __construct(Product $product)
     {
@@ -22,7 +23,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = $this->product->all();
+        //$products = $this->product->all();
+        $products = $this->product->paginate($this->totalPage);
 
         return response()->json(['data' => $products]);
     }
@@ -76,7 +78,25 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $validate = validator($data, $this->product->rules($id));
+
+        if ($validate->fails()) {
+            $messages = $validate->messages();
+
+            return response()->json(['validate.error', $messages]);
+        }
+
+        if( !$product = $this->product->find($id)) {
+            return response()->json(['error' => 'product_not_found']);
+        }
+
+        if (!$update = $product->update($data)) {
+            return response()->json(['error' => 'product_not_update'], 500);
+        }
+
+        return response()->json(['response' => $update]);
     }
 
     /**
@@ -87,6 +107,34 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if( !$product = $this->product->find($id)) {
+            return response()->json(['error' => 'product_not_found']);
+        }
+
+        if (!$delete = $product->delete()) {
+            return response()->json(['error' => 'product_not_delete'], 500);
+        }
+
+        return response()->json(['response' => $delete]);
+    }
+
+    public function search(Request $request)
+    {
+        $data = $request->all();
+
+        $validate = validator($data, $this->product->rulesSearch());
+
+        if($validate->fails()) {
+            $messages = $validate->messages();
+
+            return response()->json(['validate.error', $messages]);
+        }
+
+        $products = $this->product
+                    ->where('name', $data['key-search'])
+                    ->orWhere('description', 'LIKE', "%{$data['key-search']}%")
+                    ->paginate(5);
+
+        return response()->json(['data' => $products]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
-$this->resource('products', 'API\ProductController', ['except' => [
-    'create', 'edit' 
-]]);
+$this->group(['prefix' => 'v1'], function () {
+    $this->resource('products/search', 'Api\V1\ProductController@search');
+    $this->resource('products', 'Api\V1\ProductController', ['except' => ['create', 'edit']]);
+});
+
+
+
+
